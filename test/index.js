ff21bd4114cd50c843ea6130da4086c17d4142b7
anvaka <anvaka@gmail.com>
2016-04-12 21:46:32 -0700

    Added test for step() callback

--- test/index.js
+++ test/index.js
@@ -34,3 +34,28 @@ test('it can animate objects even when duration is 0', function(t) {
     }
   })
 });
+
+test('it notifies about each animation step', function(t) {
+  var source = {
+    x: 0
+  };
+  var target = {
+    x: 42
+  };
+
+  var invokedCount = 0;
+
+  animate(source, target, {
+    duration: 10,
+    step: function(currentValue) {
+      t.ok(currentValue === source, 'source value is passed as an argument')
+      invokedCount += 1;
+    },
+
+    done: function() {
+      t.equals(source.x, 42, 'it animated source to target');
+      t.ok(invokedCount > 0, 'It invoked step() at least once');
+      t.end();
+    }
+  })
+});
